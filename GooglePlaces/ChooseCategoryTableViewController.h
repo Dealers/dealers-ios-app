06b7a1f2f665baab273a0737619c8472983fafab
Gilad Lumbroso <gullumbroso@gmail.com>
2014-07-30 18:17:38 +0300

    1. Arranged all the Add Deal process to fit with the different screens in iOS7 iPhones 2. Adjustments to the view “Where is the Deal” and the map

---
+++ GooglePlaces/ChooseCategoryTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChooseCategoryTableViewController.h
+//  Dealers-testbeta
+//
+//  Created by Gilad Lumbroso on 7/22/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChooseCategoryTableViewController : UITableViewController
+
+@end
